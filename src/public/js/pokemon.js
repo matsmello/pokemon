5bedde214e2274b7fc1ac49ff18f3ccdd4ea7d0d
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-27 17:15:17 -0300

    Create basic structure

---
+++ src/public/js/pokemon.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const list = document.getElementById("list");
+
+button.addEventListener("click", () => {
+  alert("x");
+});
