4bd88052b87d44e0c54f773c3893dbe2c0b48006
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-27 18:05:40 -0300

    Create basic ash walked map

--- src/public/js/pokemon.js
+++ src/public/js/pokemon.js
@@ -1,7 +1,31 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
+let x = 0,
+  y = 0;
+
+let movements = {
+  N: () => y++,
+  S: () => y--,
+  E: () => x++,
+  O: () => x--,
+};
 
 button.addEventListener("click", () => {
-  alert("x");
+  if (input.value.length) {
+    const moves = input.value.toUpperCase().split("");
+
+    moves.forEach((move) => {
+      const node = document.createElement("li"); // Create a <li> node
+      const initialText = `${move} - Atual position: (${x},${y})`;
+
+      movements[move]();
+      const nextText = ` - Next position: (${x},${y})`;
+
+      const textNode = document.createTextNode(`${initialText}${nextText}`); // Create a text node
+      node.appendChild(textNode); // Append the text to <li>
+      list.appendChild(node); // Append <li> to <ul> with id="list"
+    });
+  } else {
+  }
 });
